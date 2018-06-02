20fa078a40817a1266b820a22c27114daf13e2f0
Varun Agrawal <varagrawal@gmail.com>
2018-06-01 20:16:24 -0400

    tests for the bounding boxes

--- tests/test_boundingbox.py
+++ tests/test_boundingbox.py
@@ -3,7 +3,7 @@ from bbox import BoundingBox, BoundingBoxList
 
 class TestBoundingBox(object):
     def test_basic_box(self):
-        bbox1 = BoundingBox(0, 0, 500, 500)
+        bbox1 = BoundingBox([0, 0, 500, 500])
         assert bbox1.x1 == 0
         assert bbox1.y1 == 0
         assert bbox1.x2 == 499
@@ -12,7 +12,7 @@ class TestBoundingBox(object):
         assert bbox1.w == 500
         
     def test_nonbasic_box(self):
-        bbox2 = BoundingBox(24, 48, 64, 96)
+        bbox2 = BoundingBox([24, 48, 64, 96])
 
         assert bbox2.x1 == 24
         assert bbox2.y1 == 48
--- tests/test_boundingboxlist.py
+++ tests/test_boundingboxlist.py
@@ -4,22 +4,29 @@ from bbox import BoundingBox, BoundingBoxList
 
 class TestBoundingBoxList(object):
     def test_null(self):
-        bbl = BoundingBoxList.from_bbox_list([])
-        print(bbl.shape)
+        bbl = BoundingBoxList([])
+        assert bbl.shape == (0, 4)
 
     def test_box_shapes(self):
-        l = [BoundingBox(*np.random.randint(0, 1024, size=4)) for _ in range(10)]
-        bbl = BoundingBoxList.from_bbox_list(l)
+        l = [BoundingBox(np.random.randint(0, 1024, size=4)) for _ in range(10)]
+        bbl = BoundingBoxList(l)
 
         assert len(bbl) == 10
         assert bbl.shape == (10, 4)
 
-        lx1 = np.array([b.x1 for b in l])
-        lx2 = np.array([b.x2 for b in l])
-        ly1 = np.array([b.y1 for b in l])
-        ly2 = np.array([b.y2 for b in l])
+        lx1 = np.array([b.x1 for b in l])[:, np.newaxis]
+        lx2 = np.array([b.x2 for b in l])[:, np.newaxis]
+        ly1 = np.array([b.y1 for b in l])[:, np.newaxis]
+        ly2 = np.array([b.y2 for b in l])[:, np.newaxis]
+
+        assert lx1.shape == bbl.x1.shape
+        assert ly1.shape == bbl.y1.shape
+        assert lx2.shape == bbl.x2.shape
+        assert ly2.shape == bbl.y2.shape
 
         assert np.array_equal(lx1, bbl.x1)
         assert np.array_equal(lx2, bbl.x2)
         assert np.array_equal(ly1, bbl.y1)
         assert np.array_equal(ly2, bbl.y2)
+
+        assert bbl.x1.shape == (10, 1)
