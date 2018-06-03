0840f32aac341b568ba6ccda411211f5572036c6
Varun Agrawal <varagrawal@gmail.com>
2018-06-03 17:54:40 -0400

    jaccard index accounts for possible divide by 0

--- bbox/metrics.py
+++ bbox/metrics.py
@@ -1,5 +1,8 @@
 import numpy as np
+import logging
 
+
+logger = logging.getLogger("bbox")
 
 def jaccard_index(a, b):
     """
@@ -11,11 +14,19 @@ def jaccard_index(a, b):
     xB = np.minimum(a.x2, b.x2.T)
     yB = np.minimum(a.y2, b.y2.T)
 
+    logger.debug(xA, yA, xB, yB)
+    
     intersection = np.maximum((xB - xA + 1), 0) * np.maximum((yB - yA + 1), 0)
+    logger.debug(intersection)
 
     a_area = a.width * a.height
     b_area = b.width * b.height
 
+    logger.debug(a_area, b_area)
     iou = intersection / (a_area + b_area.T - intersection)
+
+    # set nan and +/- inf to 0
+    iou[np.isinf(iou)] = 0
+    iou[np.isnan(iou)] = 0
 
     return iou
