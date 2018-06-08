4c3e42c2133c5569a5a061e5972e4b9f219baf1e
Varun Agrawal <varagrawal@gmail.com>
2018-06-08 17:59:47 -0400

    got rid of spurious print statements

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -17,7 +17,6 @@ def naive_intersection_over_union(boxA, boxB):
     yA = max(boxA[1], boxB[1])
     xB = min(boxA[2], boxB[2])
     yB = min(boxA[3], boxB[3])
-    # print(xA, yA, xB, yB)
 
     # compute the area of intersection rectangle
     interArea = (xB - xA + 1) * (yB - yA + 1)
@@ -26,9 +25,6 @@ def naive_intersection_over_union(boxA, boxB):
     # rectangles
     boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
     boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
-
-    # print(interArea)
-    # print(boxAArea, boxBArea)
 
     # compute the intersection over union by taking the intersection
     # area and dividing it by the sum of prediction + ground-truth
