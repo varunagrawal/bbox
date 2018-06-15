485f044408a154e86040167bd575b8c68e6e0e3e
Varun Agrawal <varagrawal@gmail.com>
2018-06-15 17:31:47 -0400

    renamed nms to utils so that it can hold other useful funcs as well

--- bbox/utils.py
+++ bbox/utils.py
@@ -15,25 +15,35 @@ def nms(bbl, scores, thresh):
         return np.array([]).astype(np.int)
 
     areas = bbl.w * bbl.h
-    indices = scores.argsort()[::-1]
+    order = scores.argsort()[::-1]
 
     keep = []
 
-    while indices.size > 0:
-        i = indices[0]
+    while order.size > 0:
+        i = order[0]
         keep.append(int(i))
 
-        xx1 = np.maximum(bbl.x1[i], bbl.x1[indices[1:]])
-        yy1 = np.maximum(bbl.y1[i], bbl.y1[indices[1:]])
-        xx2 = np.minimum(bbl.x2[i], bbl.x2[indices[1:]])
-        yy2 = np.minimum(bbl.y2[i], bbl.y2[indices[1:]])
+        xx1 = np.maximum(bbl.x1[i], bbl.x1[order[1:]])
+        yy1 = np.maximum(bbl.y1[i], bbl.y1[order[1:]])
+        xx2 = np.minimum(bbl.x2[i], bbl.x2[order[1:]])
+        yy2 = np.minimum(bbl.y2[i], bbl.y2[order[1:]])
+        print("Start")
+        print(i)
+        print(bbl.x1[i])
+        print(bbl.x1[i].shape)
+        print("xx1", xx1.shape)
+        print("yy1", yy1.shape)
+        print("xx2", xx2.shape)
+        print("yy2", yy2.shape)
+        print("End")
+        exit(0)
 
         w = np.maximum(0.0, xx2 - xx1 + 1)
         h = np.maximum(0.0, yy2 - yy1 + 1)
         inter = w * h
-        overlap = inter / (areas[i] + areas[indices[1:]] - inter)
+        overlap = inter / (areas[i] + areas[order[1:]] - inter)
 
         idx = np.where(overlap <= thresh)[0]
-        indices = indices[idx + 1]
+        order = order[idx + 1]
 
     return np.array(keep).astype(np.int)
