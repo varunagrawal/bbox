0440bf96674d979de4d39470d3905d36db79aef8
Varun Agrawal <varagrawal@gmail.com>
2018-06-27 17:20:00 -0400

    added properties for all points, length, width and height, and set center to default True

--- bbox/bbox3d.py
+++ bbox/bbox3d.py
@@ -7,7 +7,7 @@ class BBox3D:
     Class for 3D Bounding Boxes
     """
 
-    def __init__(self, x, y, z, length, width, height, rx=0, ry=0, rz=0, rw=1, center=False):
+    def __init__(self, x, y, z, length, width, height, rx=0, ry=0, rz=0, rw=1, center=True):
         """
         For now we just take either the center of the 3D bounding box or the top-left-closer corner,
         and the width, height and length, and quaternion values.
@@ -27,6 +27,18 @@ class BBox3D:
     @property
     def center(self):
         return np.array([self._cx, self._cy, self._cz, 1])
+
+    @property
+    def l(self):
+        return self._l
+
+    @property
+    def w(self):
+        return self._w
+
+    @property
+    def h(self):
+        return self._h
 
     @property
     def p1(self):
@@ -83,6 +95,12 @@ class BBox3D:
         p = self._q.rotate(p)
         p = p + self._c
         return np.append(p, 1)
+
+    @property
+    def p(self):
+        x = np.vstack([self.p1, self.p2, self.p3, self.p4,
+                       self.p5, self.p6, self.p7, self.p8])
+        return x
 
     # def draw_cuboid(img, p):
     #     draw = ImageDraw.Draw(img)
