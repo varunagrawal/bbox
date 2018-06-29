cf0a4b64cf1d83ecc4a0d8365c925196a8f61c5b
Varun Agrawal <varagrawal@gmail.com>
2018-06-29 11:49:55 -0400

    Handy function to draw a BBox3D given the coords in image space

--- bbox/utils.py
+++ bbox/utils.py
@@ -5,6 +5,7 @@ Adapted from Tomasz Malisiewicz's & Ross Girshick's code.
  """
 
 import numpy as np
+from PIL import ImageDraw
 
 
 def nms(bbl, scores, thresh):
@@ -16,7 +17,7 @@ def nms(bbl, scores, thresh):
 
     areas = bbl.w * bbl.h
     order = scores.argsort()[::-1]
-    
+
     keep = []
     while order.size > 0:
         i = order[0]
@@ -26,14 +27,35 @@ def nms(bbl, scores, thresh):
         yy1 = np.maximum(bbl.y1[i], bbl.y1[order[1:]])
         xx2 = np.minimum(bbl.x2[i], bbl.x2[order[1:]])
         yy2 = np.minimum(bbl.y2[i], bbl.y2[order[1:]])
-    
+
         w = np.maximum(0.0, xx2 - xx1 + 1)
         h = np.maximum(0.0, yy2 - yy1 + 1)
         inter = w * h
-        
+
         overlap = inter / (areas[i] + areas[order[1:]] - inter)
 
         idx = np.where(overlap <= thresh)[0]
         order = order[idx + 1]
 
     return np.array(keep).astype(np.int)
+
+
+def draw_cuboid(img, p):
+    draw = ImageDraw.Draw(img)
+    color = tuple(np.random.choice(range(256), size=3))
+
+    draw.line([p[0][0], p[0][1], p[1][0], p[1][1]], fill=color, width=2)
+    draw.line([p[1][0], p[1][1], p[5][0], p[5][1]], fill=color, width=2)
+    draw.line([p[5][0], p[5][1], p[4][0], p[4][1]], fill=color, width=2)
+    draw.line([p[4][0], p[4][1], p[0][0], p[0][1]], fill=color, width=2)
+
+    draw.line([p[3][0], p[3][1], p[2][0], p[2][1]], fill=color, width=2)
+    draw.line([p[2][0], p[2][1], p[6][0], p[6][1]], fill=color, width=2)
+    draw.line([p[6][0], p[6][1], p[7][0], p[7][1]], fill=color, width=2)
+    draw.line([p[7][0], p[7][1], p[3][0], p[3][1]], fill=color, width=2)
+
+    draw.line([p[0][0], p[0][1], p[3][0], p[3][1]], fill=color, width=2)
+    draw.line([p[1][0], p[1][1], p[2][0], p[2][1]], fill=color, width=2)
+    draw.line([p[5][0], p[5][1], p[6][0], p[6][1]], fill=color, width=2)
+    draw.line([p[4][0], p[4][1], p[7][0], p[7][1]], fill=color, width=2)
+    return img
