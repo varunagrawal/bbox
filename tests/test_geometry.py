8584a0d517657504eb05ebf6d59fb1098f43666a
Varun Agrawal <varagrawal@gmail.com>
2018-07-03 20:15:32 -0400

    initial tests for geometry code

---
+++ tests/test_geometry.py
@@ -0,0 +1,51 @@
+import numpy as np
+from bbox import BBox3D
+from bbox.geometry import point_plane_dist, polygon_area, polygon_intersection
+
+
+def clip(subjectPolygon, clipPolygon):
+    """
+    Naive implementation picked up from Rosetta Code. 
+    Only used for testing purposes.
+    """
+    def inside(p):
+        return(cp2[0]-cp1[0])*(p[1]-cp1[1]) > (cp2[1]-cp1[1])*(p[0]-cp1[0])
+
+    def computeIntersection():
+        dc = [cp1[0] - cp2[0], cp1[1] - cp2[1]]
+        dp = [s[0] - e[0], s[1] - e[1]]
+        n1 = cp1[0] * cp2[1] - cp1[1] * cp2[0]
+        n2 = s[0] * e[1] - s[1] * e[0]
+        n3 = 1.0 / (dc[0] * dp[1] - dc[1] * dp[0])
+        return [(n1*dp[0] - n2*dc[0]) * n3, (n1*dp[1] - n2*dc[1]) * n3]
+
+    outputList = subjectPolygon
+    cp1 = clipPolygon[-1]
+
+    for clipVertex in clipPolygon:
+        cp2 = clipVertex
+        inputList = outputList
+        outputList = []
+        s = inputList[-1]
+
+        for subjectVertex in inputList:
+            e = subjectVertex
+            if inside(e):
+                if not inside(s):
+                    outputList.append(computeIntersection())
+                outputList.append(e)
+            elif inside(s):
+                outputList.append(computeIntersection())
+            s = e
+        cp1 = cp2
+    return(outputList)
+
+
+def test_polygon_intersection():
+    a = BBox3D(0.5, 0.5, 0.5, 1, 1, 1)
+    b = BBox3D(1, 1, 1, 1, 1, 1)
+
+    i1 = clip(a.p[0: 4, 0: 2], b.p[0: 4, 0: 2])
+    i1 = np.array(i1)
+    i2 = polygon_intersection(a.p[0: 4, 0: 2], b.p[0: 4, 0: 2])
+    assert np.array_equal(i1, i2)
