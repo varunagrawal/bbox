71222752c58b8fbd96a58a812c19c8aa27252a14
Varun Agrawal <varagrawal@gmail.com>
2018-12-22 19:23:02 -0500

    updated BBox2D tests to remove shorten asserts and updated str test to match new spec

--- tests/test_bbox2d.py
+++ tests/test_bbox2d.py
@@ -64,39 +64,34 @@ class TestBBox2D(object):
     def test_x1(self):
         bbox = BBox2D([24, 48, 64, 96])
         bbox.x1 = 25
-        assert bbox.x1 == 25
-        assert bbox.w == 63
+        assert bbox.x1 == 25 and bbox.w == 63
 
     def test_x2(self):
         bbox = BBox2D([24, 48, 64, 96])
         bbox.x2 = 89
-        assert bbox.x2 == 89
-        assert bbox.w == 66
+        assert bbox.x2 == 89 and bbox.w == 66
 
     def test_y1(self):
         bbox = BBox2D([24, 48, 64, 96])
         bbox.y1 = 51
-        assert bbox.y1 == 51
-        assert bbox.h == 93
+        assert bbox.y1 == 51 and bbox.h == 93
 
     def test_y2(self):
         bbox = BBox2D([24, 48, 64, 30])
         bbox.y2 = 80
-        assert bbox.y2 == 80
-        assert bbox.h == 33
+        assert bbox.y2 == 80 and bbox.h == 33
 
     def test_w(self):
         bbox = BBox2D([24, 48, 64, 96])
         bbox.w = 70
-        assert bbox.w == 70
-        assert bbox.x1 == 24
-        assert bbox.x2 == 93
+        assert bbox.w == 70 and bbox.x1 == 24 and bbox.x2 == 93
+        bbox.width = 70
+        assert bbox.width == 70 and bbox.w == 70
 
     def test_h(self):
         bbox = BBox2D([24, 48, 64, 96])
         bbox.h = 93
-        assert bbox.h == 93
-        assert bbox.y2 == 140
+        assert bbox.h == 93 and bbox.y2 == 140
 
     def test_tolist(self):
         bbox = BBox2D([24, 48, 64, 96])
@@ -120,7 +115,7 @@ class TestBBox2D(object):
     def test_str(self):
         bbox = BBox2D([24, 48, 64, 96])
         s = repr(bbox)
-        assert "BBox2D(x=24.0, y=48.0, w=64.0, h=96.0)" == s
+        assert "BBox2D([24.0, 48.0, 64.0, 96.0])" == s
 
     def test_aspect_ratio(self):
         bbox = BBox2D([0, 0, 16, 16])
