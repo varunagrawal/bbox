30f2b605e4404134321f68bceced784d00abee6b
Varun Agrawal <varagrawal@gmail.com>
2019-01-22 19:56:25 -0500

    utils and metrics available at top-level

--- bbox/__init__.py
+++ bbox/__init__.py
@@ -1,11 +1,12 @@
 from bbox.bbox2d import BBox2D
 from bbox.bbox2d_list import BBox2DList
 from bbox.bbox3d import BBox3D
+from bbox import utils, metrics
 
 
 __author__ = "Varun Agrawal"
 __copyright__ = "GTRC & Argo AI LLC"
-__version__ = "0.8.0"
+__version__ = "0.8.1"
 __maintainer__ = "Varun Agrawal"
 __email__ = "varunagrawal@gatech.com"
 __status__ = "Beta"
