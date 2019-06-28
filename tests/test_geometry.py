a3dd01f7916b36d3dc94c35a7961a26606598bb2
Varun Agrawal <varagrawal@gmail.com>
2019-06-27 22:56:17 -0400

    improved geometry tests

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -1,18 +1,24 @@
+""""""
+
+# pylint: disable=invalid-name,missing-docstring
+
 import numpy as np
+#TODO Convert tests to Pytests
 import pytest
 from bbox import BBox3D
-from bbox.geometry import plane, point_plane_dist, polygon_area, polygon_intersection, polygon_collision, edges_of, orthogonal, is_separating_axis
+from bbox.geometry import get_plane, point_plane_dist, polygon_area, polygon_intersection, \
+    polygon_collision, edges_of, orthogonal, is_separating_axis
 
 
-def clip(subjectPolygon, clipPolygon):
+def clip(subject_polygon, clip_polygon):
     """
-    Naive implementation picked up from Rosetta Code. 
+    Naive implementation picked up from Rosetta Code.
     Only used for testing purposes.
     """
     def inside(p):
         return(cp2[0]-cp1[0])*(p[1]-cp1[1]) > (cp2[1]-cp1[1])*(p[0]-cp1[0])
 
-    def computeIntersection():
+    def compute_intersection():
         dc = [cp1[0] - cp2[0], cp1[1] - cp2[1]]
         dp = [s[0] - e[0], s[1] - e[1]]
         n1 = cp1[0] * cp2[1] - cp1[1] * cp2[0]
@@ -20,26 +26,26 @@ def clip(subjectPolygon, clipPolygon):
         n3 = 1.0 / (dc[0] * dp[1] - dc[1] * dp[0])
         return [(n1*dp[0] - n2*dc[0]) * n3, (n1*dp[1] - n2*dc[1]) * n3]
 
-    outputList = subjectPolygon
-    cp1 = clipPolygon[-1]
+    output_list = subject_polygon
+    cp1 = clip_polygon[-1]
 
-    for clipVertex in clipPolygon:
-        cp2 = clipVertex
-        inputList = outputList
-        outputList = []
-        s = inputList[-1]
+    for clip_vertex in clip_polygon:
+        cp2 = clip_vertex
+        input_list = output_list
+        output_list = []
+        s = input_list[-1]
 
-        for subjectVertex in inputList:
-            e = subjectVertex
+        for subject_vertex in input_list:
+            e = subject_vertex
             if inside(e):
                 if not inside(s):
-                    outputList.append(computeIntersection())
-                outputList.append(e)
+                    output_list.append(compute_intersection())
+                output_list.append(e)
             elif inside(s):
-                outputList.append(computeIntersection())
+                output_list.append(compute_intersection())
             s = e
         cp1 = cp2
-    return(outputList)
+    return output_list
 
 
 def test_plane():
@@ -47,7 +53,7 @@ def test_plane():
     a = np.array([1, 1, 1])
     b = np.array([-1, 1, 0])
     c = np.array([2, 0, 3])
-    assert np.array_equal(plane(a, b, c), np.array([-1, 3, 2, -4]))
+    assert np.array_equal(get_plane(a, b, c), np.array([-1, 3, 2, -4]))
 
 
 def test_point_plane_dist():
@@ -81,13 +87,13 @@ def test_is_separating_axis():
     p1, p2 = a.p[0:4, 0:2], b.p[0:4, 0:2]
     edges = edges_of(p1)
     edges += edges_of(p2)
-    
+
     # positive case
     separates, pv = is_separating_axis(orthogonal(edges[0]), p1, p2)
-    assert separates == False and pv is not None
+    assert not separates and pv is not None
 
     separates, pv = is_separating_axis(orthogonal(edges[1]), p1, p2)
-    assert separates == True and pv is None
+    assert separates and pv is None
 
 
 def test_polygon_no_collision():
@@ -96,4 +102,4 @@ def test_polygon_no_collision():
     b = BBox3D(0.1, 0.1, 0.1, 0.1, 1, 1)
     p1, p2 = a.p[0:4, 0:2], b.p[0:4, 0:2]
 
-    assert polygon_collision(p1, p2) == False
+    assert not polygon_collision(p1, p2)
