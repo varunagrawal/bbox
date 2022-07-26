ffe900aa09812cc3ea97e5b14c509b4021b8b6ab
Varun Agrawal <varagrawal@gmail.com>
2022-07-26 08:23:24 -0400

    update dependencies

--- bbox/__init__.py
+++ bbox/__init__.py
@@ -1,16 +1,15 @@
 """Main package initialization"""
 
+from bbox import metrics, utils
 from bbox.bbox2d import BBox2D
 from bbox.bbox2d_list import BBox2DList
-from bbox.box_modes import XYWH, XYXY
 from bbox.bbox3d import BBox3D
-from bbox import utils, metrics
-
+from bbox.box_modes import XYWH, XYXY
 
 __author__ = "Varun Agrawal"
 __copyright__ = "GTRC & Argo AI LLC"
-__version__ = "0.9.2"
+__version__ = "0.9.3"
 __maintainer__ = "Varun Agrawal"
-__email__ = "varunagrawal@gatech.com"
+__email__ = "varagrawal@gmail.com"
 __status__ = "Beta"
 __license__ = "MIT"
